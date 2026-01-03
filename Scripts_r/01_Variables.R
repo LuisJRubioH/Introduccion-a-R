fac8826896cc99e7e35113bd73455d798472cf79
Luis Rubio <luisjrubioh@gmail.com>
2026-01-03 17:52:11 -0500

    Creación de directorios

---
+++ Scripts_r/01_Variables.R
@@ -0,0 +1,38 @@
+# Variables
+
+x <- 86
+x
+
+# tiempo al día
+
+Oficina <- 7
+Platzi <- 1
+Transporte <- 1.5
+
+Tiempo_al_dia <- Oficina + Platzi + Transporte
+Tiempo_al_dia
+
+
+# Notas de una materia
+
+corte1 <- 0.3
+corte2 <- 0.3
+corte3 <- 0.4
+
+nota1 <- 4.0
+nota2 <- 4.6
+nota3 <- 3.0
+
+nota_c1 <- nota1 * corte1
+nota_c1
+
+nota_c2 <- nota2 * corte2
+nota_c2
+
+nota_c3 <- nota3 * corte3
+nota_c3
+
+Nota_final <- nota_c1 + nota_c2 + nota_c3
+cat("La nota final del estudiante es", Nota_final)
+
+
