e5bed3f4a79dc84e4fe552e26f0e330b55cdca6c
Luis Rubio <luisjrubioh@gmail.com>
2026-01-07 15:27:20 -0500

    Estructuras de datos - vectores

---
+++ Scripts_r/03_vectores.R
@@ -0,0 +1,10 @@
+# Estructura de datos - vectores
+
+#creamos un vector con la función c()
+vect_1 <- c(1,2,3,4)
+
+#vemos el vector creado
+vect_1
+
+#verificamos la longitud del vecor
+length(vect_1)
