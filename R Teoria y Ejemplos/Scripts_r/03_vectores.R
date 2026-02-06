7ed47a18e1add41f532d021960642fc7542d67eb
Luis Rubio <luisjrubioh@gmail.com>
2026-02-05 21:10:41 -0500

    reorganización del repositorio

---
+++ R Teoria y Ejemplos/Scripts_r/03_vectores.R
@@ -0,0 +1,19 @@
+# Estructura de datos - vectores
+
+#creamos un vector con la función c()
+vect_1 <- c(1,2,3,4)
+
+#vemos el vector creado
+vect_1
+
+#verificamos la longitud del vecor
+length(vect_1)
+
+
+# concatebar dos o más vectors
+
+v1 <- c(-2,4,6,9)
+v2 <- c(5,-5,2,0)
+
+v3 <- c(v1, v2)
+v3
--- Scripts_r/00_Hola_mundo.R
+++
@@ -1,3 +0,0 @@
-# Mi primer hola mundo desde el lenguaje de programación R
-
-print("¡Hola mundo, esto es R!")
